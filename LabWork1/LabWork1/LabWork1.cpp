510c951eed255809e5b04ce18de2a87ea37f3ce0
M1ln1chenkoBogdan <144109672+yamoneteru@users.noreply.github.com>
2023-09-19 14:01:52 +0300

    Add files via upload

---
+++ LabWork1/LabWork1/LabWork1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    const float x = 75.3, y = 0.57;
+    const char z = 2;
+
+    float nominator, denominator;
+    nominator = (y * x + (1.2 * x) / z - y / (7 * x - 3));
+    denominator = (15 * z + 1.2);
+    float result = -(nominator / denominator);
+
+    std::cout << "Result = " << result;
+    return 0;
+}
